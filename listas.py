01ffb1f7dffdc75c7588315a74c54c3cff8305a5
rColeJnr <ricardo.m.jnr@gmail.com>
2023-01-22 16:06:33 +0300

    a

--- listas.py
+++
@@ -1,7 +0,0 @@
-frase = ''
-def listas(lista):
-    for l in lista:
-        global frase
-        frase += f'{l}, '
-    print(frase)
-listas(["a", 'b', 'c'])
--- spam.py
+++
@@ -1,5 +0,0 @@
-print ('Hello world!')
-print ('What is your name?') # Ask for their name
-myName = input()
-print(f'nice to meet u {myName}')
-print(f'The length of ur name is {len(myName)}')
